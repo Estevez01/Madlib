9ee76c68594b7fa6ab4f560c71d07d7669e8b23f
Eduardo Estevez <eduardo.estevezjr@outlook.com>
2022-03-03 09:24:09 -0800

    Add project files.

---
+++ Madlib/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Madlib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, number, noun, color;
+            Console.Write("Enter a name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            number = Console.ReadLine();
+
+            Console.Write("Enter a noun: ");
+            noun = Console.ReadLine();
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+            Console.WriteLine("I am " + number + " years old");
+            Console.WriteLine("My favorite " + noun + " is " + color);
+
+            Console.ReadLine();
+        }
+    }
+}
