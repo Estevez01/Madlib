f4d8bb0743b166b711f76e41fb1acb99401f12ca
Estevez01 <98117509+Estevez01@users.noreply.github.com>
2022-03-03 10:23:20 -0800

    Update Program.cs

--- Madlib/Program.cs
+++ Madlib/Program.cs
@@ -1,27 +1,42 @@
-﻿using System;
+using System;
+
 namespace Madlib
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string name, number, noun, color;
-            Console.Write("Enter a name: ");
-            name = Console.ReadLine();
+            bool endApp = false;
+            Console.WriteLine("Let's play Madlibs!");
 
-            Console.Write("Enter a number: ");
-            number = Console.ReadLine();
+            while (!endApp)
+            {
 
-            Console.Write("Enter a noun: ");
-            noun = Console.ReadLine();
+                string animal, ride, color, noun, foodPlural;
 
-            Console.Write("Enter a color: ");
-            color = Console.ReadLine();
+                Console.Write("Enter a Animal: ");
+                animal = Console.ReadLine();
 
-            Console.WriteLine("Hello, " + name);
-            Console.WriteLine("I am " + number + " years old");
-            Console.WriteLine("My favorite " + noun + " is " + color);
+                Console.Write("Enter a Ride: ");
+                ride = Console.ReadLine();
 
+                Console.Write("Enter a color: ");
+                color = Console.ReadLine();
+
+                Console.Write("Enter a noun: ");//A noun is a person, place or thing
+                noun = Console.ReadLine();
+
+                Console.Write("Enter a food(Plural): ");
+                foodPlural = Console.ReadLine();
+
+                Console.WriteLine("Today I went to my favorite Taco Stand called the " + animal);
+                Console.WriteLine("Unlike most food stands, they cook and prepare the food in a " + ride);
+                Console.WriteLine("The best thing on the menu is the " + color + " " + noun);
+                Console.WriteLine("Instead of ground beef they fill the taco with " + foodPlural);
+
+                Console.Write("Press 'e' and Enter to close the app, or press any other key and Enter to continue: ");//User chooses if they want to play again
+                if (Console.ReadLine() == "e") endApp = true;
+            }
             Console.ReadLine();
         }
     }
